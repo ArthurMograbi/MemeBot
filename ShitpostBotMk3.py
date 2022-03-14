3a23d08d9eeb238ccf0cdc907b63cf1830943691
Arthur Mograbi <arthurmograbi@gmail.com>
2022-03-14 18:13:53 -0300

    Added automatic directory creation and made general code improvements

--- ShitpostBotMk3.py
+++ ShitpostBotMk3.py
@@ -5,27 +5,54 @@ from random import seed
 import os, os.path
 from datetime import datetime
 import time
-seed(datetime.now())
-file_path=os.getcwd()
-#objnumb=len(os.listdir(file_path+'\\object'))
-#tempnumb=len(os.listdir(file_path+'\\template'))
 
-dataFile =open(file_path+"\\data.txt","r")
+
+
+
+def try_mkdir(dirName):
+    try:
+        os.mkdir(dirName)
+        print("Directory %s Created " % dirName) 
+    except FileExistsError:
+        pass
+
+def try_mkfile(fileName):
+    try:
+        open(fileName,'x')
+        print("File %s Created " % fileName) 
+    except FileExistsError:
+        pass
+
+
+try_mkdir("./Object")
+try_mkdir("./Template")
+try_mkdir("./TemplateBack")
+try_mkdir("./OUTPUT")
+
+try_mkfile("./data.txt")
+try_mkfile("./existing.txt")
+
+seed(datetime.now())
+
+dataFile =open("./data.txt","r")
 data = dataFile.read().split("\n")
+
+dataFile.close()
+
 data = [i.split(",") for i in data]
 data = [ [int(j) for j in i if j!="" if i[0][0]!="M"] for i in data]
-#print(data)
-#seed(os.time())
+
+
 
 def craft(chotemp,choobj):
     tem2=False
-    obj = Image.open(file_path+'\\Object/'+str(choobj)+'.png').convert("RGBA")
+    obj = Image.open('./Object/'+str(choobj)+'.png').convert("RGBA")
     if data[chotemp][5]==1:
         filetype='.jpg'
     else:
         filetype='.png'
-        tem2= Image.open(file_path+'\\TemplateBack/'+str(chotemp)+filetype).convert("RGBA")
-    tem = Image.open(file_path+'\\Template/'+str(chotemp)+filetype).convert("RGBA")
+        tem2= Image.open('./TemplateBack/'+str(chotemp)+filetype).convert("RGBA")
+    tem = Image.open('./Template/'+str(chotemp)+filetype).convert("RGBA")
     box = (data[chotemp][0],data[chotemp][1],data[chotemp][2],data[chotemp][3])
     region = obj.resize((data[chotemp][2]-data[chotemp][0],data[chotemp][3]-data[chotemp][1]))
     region = region.rotate(data[chotemp][4])
@@ -33,18 +60,18 @@ def craft(chotemp,choobj):
     if tem2:
         width, height = tem.size
         tem.paste(tem2,(0,0,width,height),mask=tem2)
-    tem.save(file_path+'\\OUTPUT/'+str(len(os.listdir(file_path+'\\OUTPUT')))+'.png')
+    tem.save('./OUTPUT/'+str(len(os.listdir('./OUTPUT')))+'.png')
         
 
 
 def gen(num):
     print("Processing\n|",end='')
     for i in range(num):
-        eFile =open(file_path+"\\existing.txt","r+")
+        eFile =open("./existing.txt","r+")
         exis = eFile.read()
         exisM = exis.split(",")
-        objLen=len(os.listdir(file_path+'\\Object'))
-        tempLen=len(os.listdir(file_path+'\\Template'))
+        objLen=len(os.listdir('./Object'))
+        tempLen=len(os.listdir('./Template'))
         if len(exisM) >= objLen*tempLen:
            raise Exception("All possible combinations exausted!")
         oNum= randint(1,objLen)
@@ -63,4 +90,4 @@ def gen(num):
 
 gen(int(input("How many would you like to generate?\n")))
 #craft(5,2)
-dataFile.close()
+
